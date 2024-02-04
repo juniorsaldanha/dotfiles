cb92c599d708e40736f06474967d7b2e8f100dd6
Junior Saldanha <esaldanha@ionic.health>
2024-02-04 00:51:02 -0300

    added schemastore link to yamlls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    servers = {
+      yamlls = {
+        validate = true,
+        schemaStore = {
+          enable = true,
+          url = "https://www.schemastore.org/api/json/catalog.json",
+        },
+      },
+    },
     setup = {
       clangd = function(_, opts)
         opts.capabilities.offsetEncoding = { "utf-16" }
