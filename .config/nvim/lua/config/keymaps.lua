cae4e71b39f02a40d99050598710a8cfc150d9ac
Junior Saldanha <esaldanha@ionic.health>
2024-02-24 17:21:44 -0300

    keymaps: changed a little bit

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,67 +4,62 @@ local k = vim.keymap
 vim.g.mapleader = " "
 
 -- jk to escape
-k.set("i", "jk", "<esc>", { noremap = true, silent = true })
+k.set("i", "jk", "<esc>", { desc = "Escape it from insert mode", noremap = true, silent = true })
 
 -- <S-h/l> to go to the beginning/end of the line (non white-space)
-k.set("n", "<S-h>", "^", { noremap = true, silent = true })
-k.set("n", "<S-l>", "$", { noremap = true, silent = true })
+k.set("n", "<S-h>", "^", { desc = "Go to first char on line", noremap = true, silent = true })
+k.set("n", "<S-l>", "$", { desc = "Go to last char on line", noremap = true, silent = true })
 
 -- <C-h/l> to move to the window to the left/right
-k.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-k.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+k.set("n", "<C-h>", "<C-w>h", { desc = "Go to left pane", noremap = true, silent = true })
+k.set("n", "<C-l>", "<C-w>l", { desc = "Go to right pane", noremap = true, silent = true })
 
 -- <C-j/k> to move to the windows to the up/down
-k.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
-k.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+k.set("n", "<C-j>", "<C-w>j", { desc = "Go to down pane", noremap = true, silent = true })
+k.set("n", "<C-k>", "<C-w>k", { desc = "Go to up pane", noremap = true, silent = true })
 
 -- Move selected line / block of text in visual mode
-k.set("x", "K", ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
 k.set("x", "J", ":move '>+1<CR>gv-gv", { noremap = true, silent = true })
+k.set("x", "K", ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
 
--- Close buffer with <leader>bd
+-- Close  with <leader>bd
 k.set(
   "n",
   "<leader>bd",
-  "<cmd>bd<CR>",
-  { desc = "Close current buffer", noremap = true, silent = true }
+  ":bd<CR>",
+  { desc = "Close current ", noremap = true, silent = true }
 )
--- Delete buffer FORCE with <leader>bD
+-- Delete  FORCE with <leader>bD
 k.set(
   "n",
   "<leader>bD",
-  "<cmd>bd!<CR>",
-  { desc = "Close current buffer", noremap = true, silent = true }
+  ":bd!<CR>",
+  { desc = "Close current  FORCED", noremap = true, silent = true }
 )
--- Close all buffers except the current one with <leader>ba
-k.set("n", "<leader>ba", "<cmd>bufdo bd %<CR>", {
-  desc = "Close all buffers except the current one",
+-- Close all s except the current one with <leader>ba
+k.set("n", "<leader>ba", ":bufdo bd %<CR>", {
+  desc = "Close all s except the current one",
   noremap = true,
   silent = true,
 })
 
--- Windows resizing with <C-w> and h/j/k/l repetly and Descriptions to WhichKey
 k.set(
   "n",
-  "<C-w-h>",
-  "<C-w><",
-  { desc = "Resize window left", noremap = true, silent = true }
+  "<leader>sc",
+  ":noh<CR>",
+  { desc = "Clear search highlights", noremap = true, silent = true }
 )
+ 
 k.set(
   "n",
-  "<C-w>j",
-  "<C-w>-",
-  { desc = "Resize window down", noremap = true, silent = true }
+  "<leader>sr",
+  ":%s//g<Left><Left>",
+  { desc = "Search and replace", noremap = true, silent = true }
 )
+
 k.set(
   "n",
-  "<C-w>k",
-  "<C-w>+",
-  { desc = "Resize window up", noremap = true, silent = true }
-)
-k.set(
-  "n",
-  "<C-w>l",
-  "<C-w>>",
-  { desc = "Resize window right", noremap = true, silent = true }
+  "<leader>ss",
+  ":set spell!<CR>",
+  { desc = "Toggle spell check", noremap = true, silent = true }
 )
