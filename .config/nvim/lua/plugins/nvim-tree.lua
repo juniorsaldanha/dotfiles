e5182430741db1e52dc7584452ab87f0a6e26c02
Junior Saldanha <esaldanha@ionic.health>
2024-02-24 17:41:38 -0300

    fix: nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,33 +1,35 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "1.0",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("nvim-tree").setup({
-			sort = {
-				sorter = "case_sensitive",
-			},
-			view = {
-				width = 40,
-				side = "left",
-			},
-			renderer = {
-				indent_markers = {
-					enable = true,
-				},
-			},
-			filters = {
-				dotfiles = true,
-			},
-		})
+  "nvim-tree/nvim-tree.lua",
+  version = "1.0",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local nvim_tree_api = require("nvim-tree.api")
+    local nvim_tree = require("nvim-tree")
+    nvim_tree.setup({
+      sort = {
+        sorter = "case_sensitive",
+      },
+      view = {
+        width = 40,
+        side = "left",
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+      },
+      filters = {
+        dotfiles = true,
+      },
+    })
 
-		vim.keymap.set(
-			"n",
-			"<leader>e",
-			"<cmd>NvimTreeToggle<CR>",
-			{ desc = "Nvim Tree Toggle", noremap = true, silent = true }
-		)
-	end,
+    vim.keymap.set(
+      "n",
+      "<leader>e",
+      "<cmd>NvimTreeToggle<CR>",
+      { desc = "NvimTree Toggle", noremap = true, silent = true }
+    )
+  end,
 }
