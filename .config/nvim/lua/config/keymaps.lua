bf480b01be517cb4ab5914e0a9049a45ebeac868
Junior Saldanha <umsaldanha@gmail.com>
2024-04-20 10:53:18 -0300

    fix: these keymaps are being set in vim-tmux-navitador pluging

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -12,12 +12,6 @@ map("i", "jk", "<esc>")
 map("n", "<S-h>", "^", opts("Go first char on line"))
 map("n", "<S-l>", "$", opts("Go first char on line"))
 
--- Move between panes
-map("n", "<C-h>", "<C-w><C-h>")
-map("n", "<C-l>", "<C-w><C-l>")
-map("n", "<C-j>", "<C-w><C-j>")
-map("n", "<C-k>", "<C-w><C-k>")
-
 -- Buffers
 map("n", "<leader>bn", "<cmd>bn<cr>", opts("[B]uffer [N]ext Buffer"))
 map("n", "<leader>bp", "<cmd>bp<cr>", opts("[B]uffer [P]revious Buffer"))
@@ -26,3 +20,6 @@ map("n", "<leader>ba", "<cmd>bufdo bd<cr>", opts("[B]uffer Delete [A]ll Buffers"
 
 -- LazyGit
 map("n", "<leader>gl", "<cmd>LazyGit<CR>", opts("[G]it [L]azyGit"))
+
+-- Search and Replace on whole buffer
+map("n", "<leader>sr", ":%s//g<left><left>", opts("[S]earch and [R]eplace"))
