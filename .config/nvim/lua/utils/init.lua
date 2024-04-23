4b745bd28b08628e909840584a0827dfd9502c92
Junior Saldanha <umsaldanha@gmail.com>
2024-04-23 02:22:23 -0300

    fix(utils): nothing really, just setting a pattern for returing function using init.lua

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -1,11 +1,9 @@
-local M = {}
+-- Utility
+local M = require("util")
 
 -- Deprecated functions
 M.getWindows = require("util").GetWindows
 M.require = require("util").require
-
--- Utility
-M.util = require("util")
 
 -- Configuration
 M.config = require("config")
