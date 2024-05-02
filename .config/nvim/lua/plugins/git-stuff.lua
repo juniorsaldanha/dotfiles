be8d4f6b26a9f16e1c70f0f3d71871dd93d228ae
Junior Saldanha <umsaldanha@gmail.com>
2024-05-01 23:17:13 -0300

    feat(plugin): adding gitsings plugin

--- .config/nvim/lua/plugins/git-stuff.lua
+++ .config/nvim/lua/plugins/git-stuff.lua
@@ -10,4 +10,10 @@ return {
       "nvim-lua/plenary.nvim",
     },
   }, { "tpope/vim-fugitive", lazy = false },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end
+  }
 }
