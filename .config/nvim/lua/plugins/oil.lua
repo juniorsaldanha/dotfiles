125fb6612091fe4a85ed94e0d062a8a0b86416cd
Junior Saldanha <umsaldanha@gmail.com>
2024-05-15 20:26:53 -0300

    refactor(oil): changed oil configuration

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,12 +1,25 @@
 return {
   "stevearc/oil.nvim",
   version = "*",
-  opts = {
-    columns = {
-      "icon",
-    },
-  },
-  keys = {
-    { "<leader>o", "<cmd>Oil<cr>", desc = "Toggle Oil" },
-  },
+  config = function()
+    require("oil").setup({
+      columns = { "icon" },
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-l>"] = false,
+        ["<C-j>"] = false,
+        ["<C-k>"] = false,
+        ["<M-h>"] = "actions.select_split",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    local map = vim.keymap.set
+    local opts = function(desc)
+      return { noremap = true, silent = true, desc = desc }
+    end
+    map("n", "<leader>o", "<cmd>Oil<cr>", opts("Toggle Oil"))
+    map("n", "<leader>-", require("oil").toggle_float, opts("Toggle Oil (float)"))
+  end
 }
