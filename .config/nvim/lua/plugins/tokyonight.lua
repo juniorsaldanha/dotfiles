53d60a358cc3583450cd620d6616db8ec9681b43
Junior Saldanha <umsaldanha@gmail.com>
2024-05-15 22:21:02 -0300

    feat(transparency-nvim): added config and usercmd to toggle transparency of tokyonight

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,20 +1,40 @@
+local config = require("utils.config")
+local M = {}
+M.toggle_transparent = function()
+  config.Toggle_transparent()
+  vim.notify(
+    "Transparency "
+    .. (config.Get_transparent() and "Enabled" or "Disabled"),
+    vim.log.levels.INFO,
+    { title = "Transparency Toggle" }
+  )
+  vim.cmd("Lazy reload tokyonight.nvim")
+end
+
 return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  -- opts = {},
   config = function()
     local tokyo = require("tokyonight")
     tokyo.setup({
       style = "night",
       light_style = "day",
       terminal_colors = true,
-      transparent = false,
+      transparent = config.Get_transparent(),
       on_colors = function(colors)
         colors.error = colors.red
         colors.hint = colors.orange
       end,
     })
     vim.cmd("colorscheme tokyonight")
+
+    local usercmd = vim.api.nvim_create_user_command
+    usercmd("ToggleTransparency", M.toggle_transparent, {
+      desc = "Toggle Transparency",
+      nargs = "?"
+    })
   end,
+
 }
