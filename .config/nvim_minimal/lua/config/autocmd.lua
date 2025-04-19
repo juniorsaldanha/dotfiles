3119f91bf28d3e698163253c09163cea49a8c9b8
Junior Saldanha <umsaldanha@gmail.com>
2025-04-19 05:04:06 -0300

    nvim_minimal update fixed auto complete

--- .config/nvim_minimal/lua/config/autocmd.lua
+++ .config/nvim_minimal/lua/config/autocmd.lua
@@ -9,7 +9,7 @@ local CustomGroup = augroup("Saldanha", {})
 autocmd("TextYankPost", {
   group = YankGroup,
   callback = function()
-    vim.highlight.on_yank({ timeout = "150" })
+    vim.hl.on_yank({ timeout = "150" })
   end,
 })
 
@@ -28,60 +28,5 @@ autocmd("VimResized", {
     local current_tab = vim.fn.tabpagenr()
     vim.cmd("tabdo wincmd =")
     vim.cmd("tabnext " .. current_tab)
-  end,
-})
-
--- LSP keybindings
--- see `:h lsp`
-autocmd("LspAttach", {
-  group = CustomGroup,
-  callback = function(e)
-    local opts = function(desc)
-      return { desc = desc, noremap = true, silent = true, buffer = e.buf }
-    end
-    local map = vim.keymap.set
-    -- local lspbuf = vim.lsp.buf
-    local lspdiag = vim.diagnostic
-    -- map("n", "gd", function()
-    --   lspbuf.definition()
-    -- end, opts("[LSP] Go to definition"))
-    --
-    -- map("n", "K", function()
-    --   lspbuf.hover()
-    -- end, opts("[LSP] Hover"))
-    --
-    -- map("n", "<leader>la", function()
-    --   lspbuf.code_action()
-    -- end, opts("[LSP] Code action"))
-    --
-    -- map("n", "<leader>lR", function()
-    --   lspbuf.references()
-    -- end, opts("[LSP] References"))
-    --
-    -- map("n", "<leader>lr", function()
-    --   lspbuf.rename()
-    -- end, opts("[LSP] Rename"))
-    -- map("n", "<leader>li", function()
-    --   lspbuf.implementation({})
-    -- end, opts("[LSP] Implementation"))
-    -- map("n", "<leader>lf", function()
-    --   lspbuf.format()
-    -- end, opts("[LSP] Format"))
-    --
-    -- map("i", "<C-h>", function()
-    --   lspbuf.signature_help()
-    -- end, opts("[LSP] Signature help"))
-    --
-    -- map("n", "<leader>ld", function()
-    --   lspdiag.open_float()
-    -- end, opts("[LSP] Diagnostics"))
-
-    map("n", "d[", function()
-      lspdiag.goto_next()
-    end, opts("[LSP] Next diagnostic"))
-
-    map("n", "d]", function()
-      lspdiag.goto_prev()
-    end, opts("[LSP] Previous diagnostic"))
   end,
 })
