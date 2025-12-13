dd9e77061e86a8f86384543c274d1f3ae5595320
Junior Saldanha <umsaldanha@gmail.com>
2025-12-13 18:22:27 -0300

    treesitter: updated config for treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,53 +5,53 @@
 ]]
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-        -- ====================================================================
-        -- PARSERS TO AUTO-INSTALL
-        -- Add or remove languages here
-        -- ====================================================================
-        ensure_installed = {
-            "bash",
-            "c",
-            "css",
-            "go",
-            "html",
-            "javascript",
-            "jsdoc",
-            "json",
-            "lua",
-            "luadoc",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "rust",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "yaml",
-        },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        },
-        indent = {
-            enable = true,
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>",
-            },
-        },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    -- ====================================================================
+    -- PARSERS TO AUTO-INSTALL
+    -- Add or remove languages here
+    -- ====================================================================
+    ensure_installed = {
+      "bash",
+      "c",
+      "css",
+      "go",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "rust",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
     },
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-    end,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
