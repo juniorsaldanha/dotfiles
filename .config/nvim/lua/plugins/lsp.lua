e68a1261788c73a1a84474a63e6ae68849f63132
Junior Saldanha <umsaldanha@gmail.com>
2025-12-13 18:21:41 -0300

    lsp: adding nvim RUNTIME to lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -140,7 +140,18 @@ return {
         lua_ls = {
           settings = {
             Lua = {
-              workspace = { checkThirdParty = false },
+              -- Tell the language server which version of Lua you're using
+              runtime = { version = "LuaJIT" },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                  -- Depending on the usage, you might want to add specific plugins here too
+                  -- "${3rd}/luv/library"
+                  -- "${3rd}/busted/library",
+                },
+              },
               telemetry = { enable = false },
               hint = { enable = true },
             },
