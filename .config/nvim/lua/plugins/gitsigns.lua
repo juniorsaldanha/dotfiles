cbb7387d9b0257dda5f7e57e6450d98e6ae9cc65
Junior Saldanha <umsaldanha@gmail.com>
2025-12-14 03:12:29 -0300

    gitsigns: updated, and decreased delay of current blame inline

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -11,10 +11,20 @@ return {
     signs = {
       add = { text = "┃" },
       change = { text = "┃" },
-      delete = { text = "━" },
-      topdelete = { text = "━" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
       changedelete = { text = "~" },
-      untracked = { text = "║" },
+      untracked = { text = "┆" },
+    },
+    auto_attach = true,
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame` or <leader>tb
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 100,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+      use_focus = true,
     },
     -- ====================================================================
     -- KEYMAPS
