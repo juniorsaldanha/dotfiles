158b6896f9952e90b0eefcddcdf0fb78cfeaa4b4
Junior Saldanha <umsaldanha@gmail.com>
2026-01-23 11:06:04 -0300

    chore: updating some stuff

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -6,53 +6,53 @@
 ]]
 
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lint = require("lint")
 
-    -- ====================================================================
-    -- LINTERS BY FILETYPE
-    -- Add or modify linters here
-    -- ====================================================================
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      python = { "pylint" },
-      go = { "golangcilint" },
-      -- Add more linters as needed
-    }
+        -- ====================================================================
+        -- LINTERS BY FILETYPE
+        -- Add or modify linters here
+        -- ====================================================================
+        lint.linters_by_ft = {
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            javascriptreact = { "eslint_d" },
+            typescriptreact = { "eslint_d" },
+            python = { "pylint" },
+            go = { "golangcilint" },
+            -- Add more linters as needed
+        }
 
-    -- CUSTOM CONFIG FOR GOLANGCI-LINT
-    -- By default, nvim-lint runs on the specific file.
-    -- We override it to run on the directory of the current file.
-    local golangcilint = lint.linters.golangcilint
-    golangcilint.args = {
-      "run",
-      "--out-format",
-      "json",
-      "--show-stats=false",
-      "--print-issued-lines=false",
-      "--print-linter-name=false",
-    }
+        -- CUSTOM CONFIG FOR GOLANGCI-LINT
+        -- By default, nvim-lint runs on the specific file.
+        -- We override it to run on the directory of the current file.
+        -- local golangcilint = lint.linters.golangcilint
+        -- golangcilint.args = {
+        --   "run",
+        --   "--out-format",
+        --   "json",
+        --   "--show-stats=false",
+        --   "--print-issued-lines=false",
+        --   "--print-linter-name=false",
+        -- }
 
-    -- Create autocommand to trigger linting
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        -- Create autocommand to trigger linting
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
 
-    -- ====================================================================
-    -- KEYMAPS
-    -- ====================================================================
-    vim.keymap.set("n", "<leader>cl", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting" })
-  end,
+        -- ====================================================================
+        -- KEYMAPS
+        -- ====================================================================
+        vim.keymap.set("n", "<leader>cl", function()
+            lint.try_lint()
+        end, { desc = "Trigger linting" })
+    end,
 }
