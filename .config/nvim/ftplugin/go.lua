ed75fd0850e111c0d616bf3439ca339ed8428ce1
Junior Saldanha <umsaldanha@gmail.com>
2026-01-29 15:08:50 -0300

    feat(nvim): ftplugin for go

--- .config/nvim/ftplugin/go.lua
+++ .config/nvim/ftplugin/go.lua
@@ -1,3 +1,19 @@
-print("ftplugin for Go loaded")
+-- Set the column guide
+vim.opt_local.colorcolumn = "120"
 
-vim.opt.colorcolumn = "120"
+-- Go-specific indentation (Standard Go uses tabs, not spaces)
+vim.opt_local.expandtab = false
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+
+-- Optional: Auto-format on save using LSP
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        -- This calls the LSP formatting sync
+        vim.lsp.buf.format({ async = false })
+
+        -- Optional: If you use 'goimports', you can add logic here
+        -- to organize imports automatically.
+    end,
+})
